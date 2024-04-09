e1ee6e635c0c5cf04b768f12205b5760c3068ae6
nvanschaack <nellvanschaack@gmail.com>
2024-04-08 21:08:31 -0500

    added light toggle to js, added light/dark style to css, cleaned up attributes in html

---
+++ assets/script/blog.js
@@ -0,0 +1,7 @@
+// Form Submission Handling: When the user submits the form with a new blog post, you need to handle the form submission using JavaScript. You can capture the form data (such as title, content, author, etc.) when the form is submitted.
+
+// Storing Blog Posts: You can store the submitted blog post data in an array, local storage, or a database depending on your setup and requirements.
+
+// Redirect to New Page: After successfully submitting the form and storing the blog post data, you can redirect the user to a new page where the blog posts will be displayed.
+
+// Displaying Blog Posts: On the new page, you can dynamically render the blog posts from the stored data. You can use JavaScript to create HTML elements for each blog post and display them on the page.
--- assets/script/form.js
+++ assets/script/form.js
@@ -1,19 +1,46 @@
 //when i type a username etc. in the form label, it will console log that username
 const userName = document.getElementById('username');
 const title = document.getElementById('title');
-const blogMessage = document.getElementById('msg');
+const blogMessage = document.getElementById('text-area');
 const submit = document.getElementById('submit');
 
-submit.addEventListener('click', function(event){
+submit.addEventListener('click', function (event) {
     event.preventDefault();
 
-    const blogPost ={
+    const blogPost = {
         username: userName.value,
         title: title.value,
         content: blogMessage.value,
-
-        localStorage.setItem('stored',userName.value, title.value, blogMessage.value)
     };
-    // console.log(blogPost) -use activity 23
-})
 
+    const saveToLocalStorage = function () {
+        const blogPostString = JSON.stringify(blogPost);
+        localStorage.setItem('storedBlogPost', blogPostString);
+    };
+    saveToLocalStorage();
+    console.log(JSON.parse(localStorage.getItem('storedBlogPost')));
+//have the form revert back to blank inputs when i click submit -ADD TO LINE 14? EX ON 04.15
+    submit.addEventListener('click', function(){
+        userName.value= '';
+        title.value= '';
+        blogMessage.value= '';
+
+    })
+})
+//LIGHT SWITCH
+const lightSwitch = document.querySelector('.button')
+const container = document.querySelector('.container')
+//set default to light
+ let mode = 'light';
+//listen for a click event on toggle switch
+lightSwitch.addEventListener('click',function(){
+    //if mode is light, apply dark background
+    if (mode === 'light'){
+        mode = 'dark';
+        container.setAttribute('class', 'dark')
+    } else {
+        mode = 'light';
+        container.setAttribute('class', 'light')
+    }
+    console.log(container)
+})
